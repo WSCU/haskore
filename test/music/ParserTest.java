549b2ccc9e9eb36e153f61edb42c901a7a4a6011
brettsargent1 <brettsargent1@91e22212-0c79-11df-8ecc-dda264a263c0>
2010-02-09 22:42:33 +0000

    Added several more tests for Parser.parseAPat

--- test/music/ParserTest.java
+++ test/music/ParserTest.java
@@ -1,8 +1,3 @@
-/*
- * To change this template, choose Tools | Templates
- * and open the template in the editor.
- */
-
 package music;
 
 import java.util.ArrayList;
@@ -41,8 +36,6 @@ public class ParserTest {
     /**
      * Test of setToks method, of class Parser.
      */
-    
-
     /**
      * Test of parseDecls method, of class Parser.
      */
@@ -57,11 +50,10 @@ public class ParserTest {
         TokenStream t = Lexer.tokens;
         ArrayList<Decl> expResult = null;
         ArrayList<Decl> result = Parser.parseDecls(t);
-        for(Decl i : result)
-        {
+        for (Decl i : result) {
             System.out.println(i.print());
         }
-        
+
         assertEquals(expResult, result);
         // TODO review the generated test code and remove the default call to fail.
         fail("The test case is a prototype.");
@@ -93,62 +85,158 @@ public class ParserTest {
         fail("The test case is a prototype.");
     }
 
-       /**
-     * This test fails. It is not yet known if this is because of a bug.
-     * The parseAPat method calls the next() method on its TokenStream toks.
-     * Since our token is in column 0 the next method returns a Token.semicolon
-     * The semicolon causes the ParsePat method to hit its else statement and
-     * generate a ParseError
-     */
-    @Test
-    public void testParsePat2() {
-        System.out.println("Testing parseAPat");
-        Pat expResult = new PatConst(new Token(Symbol.toSymbol("5"), "5", new Place("testParseAPat", 0, 0), TokenType.numberToken));
-        TokenStream tokens = new TokenStream();
-        tokens.addToken(new Token(Symbol.toSymbol("5"), "5", new Place("testParseAPat", 0, 0), TokenType.numberToken));
-        Parser.setToks(tokens);
-        Pat result = Parser.parseAPat();
-        compareParsedObjects(expResult, result);
-    }
-
     /**
-     * Test of parsePat method, of class Parser.
+     * Test of parsePat method, with a numberToken.
      */
     @Test
     public void testParsePat() {
-        System.out.println("Testing parseAPat");
-        Pat expResult = new PatConst(new Token(Symbol.toSymbol("5"), "5", new Place("testParseAPat", 0, 1), TokenType.numberToken));
+        System.out.println("Testing parsePat");
+        Pat expResult = new PatConst(new Token(Symbol.toSymbol("5"), "5", new Place("testParsePat", 0, 1), TokenType.numberToken));
         TokenStream tokens = new TokenStream();
-        tokens.addToken(new Token(Symbol.toSymbol("5"), "5", new Place("testParseAPat", 0, 1), TokenType.numberToken));
-        Parser.setToks(tokens);
-        Pat result = Parser.parsePat();
-        compareParsedObjects(expResult, result);
-    }
-
-
-    //Why is this expecting a patConst???
-    @Test
-    public void testParsePat3() {
-        System.out.println("Testing parseAPat");
-        Pat expResult = new PatVar(new Token(Symbol.toSymbol("a"), "a", new Place("testParseAPat", 0, 1), TokenType.varToken));
-        TokenStream tokens = new TokenStream();
-        tokens.addToken(new Token(Symbol.toSymbol("a"), "a", new Place("testParseAPat", 0, 1), TokenType.varToken));
+        tokens.addToken(new Token(Symbol.toSymbol("5"), "5", new Place("testParsePat", 0, 1), TokenType.numberToken));
         Parser.setToks(tokens);
         Pat result = Parser.parsePat();
         compareParsedObjects(expResult, result);
     }
 
     /**
-     * Test of parseExp method, of class Parser.
+     * The parseAPat method calls the next() method on its TokenStream toks.
+     * Since our token is in column 0 the next method returns a Token.semicolon
+     * The semicolon causes the ParsePat method to hit its else statement and
+     * generate a ParseError. This is believed to be the correct behavior.
      */
     @Test
-    public void testParseExp() {
-        System.out.println("parseExp");
-        Exp expResult = null;
-        Exp result = Parser.parseExp();
-        assertEquals(expResult, result);
-        // TODO review the generated test code and remove the default call to fail.
-        fail("The test case is a prototype.");
+    public void testParsePat2() {
+        System.out.println("Testing parsePat2");
+        Pat expResult = new PatConst(new Token(Symbol.toSymbol("5"), "5", new Place("testParsePat", 0, 0), TokenType.numberToken));
+        TokenStream tokens = new TokenStream();
+        tokens.addToken(new Token(Symbol.toSymbol("5"), "5", new Place("testParsePat", 0, 0), TokenType.numberToken));
+        Parser.setToks(tokens);
+        try{
+            Parser.parseAPat();
+        }
+        catch (ParseError e){
+            return;
+        }
+        fail("The program should have generated an exception");
+    }
+
+    /**
+     * Tests the parsePat method with a varToken
+     */
+    @Test
+    public void testParsePat3() {
+        System.out.println("Testing parsePat3");
+        Pat expResult = new PatVar(new Token(Symbol.toSymbol("a"), "a", new Place("testParsePat", 0, 1), TokenType.varToken));
+        TokenStream tokens = new TokenStream();
+        tokens.addToken(new Token(Symbol.toSymbol("a"), "a", new Place("testParsePat", 0, 1), TokenType.varToken));
+        Parser.setToks(tokens);
+        Pat result = Parser.parsePat();
+        compareParsedObjects(expResult, result);
+
+    }
+
+    /**
+     * Tests the parsePat method with an underscore token
+     */
+    @Test
+    public void testParsePat4() {
+        System.out.println("Testing parsePat4");
+        Pat expResult = new PatAny();
+        TokenStream tokens = new TokenStream();
+        tokens.addToken(new Token(Symbol.toSymbol("_"), "_", new Place("testParsePat4", 0, 1), TokenType.puncToken));
+        Parser.setToks(tokens);
+        Pat result = Parser.parsePat();
+        compareParsedObjects(expResult, result);
+
+    }
+
+    /**
+     * Test parsePat with token stream containing an var surrounded in parens
+     */
+    @Test
+    public void testParsePat5() {
+        System.out.println("Testing parsePat5");
+        Pat expResult = new PatVar(new Token(Symbol.toSymbol("f"), "f", new Place("testParsePat", 0, 2), TokenType.varToken));
+        TokenStream tokens = new TokenStream();
+        tokens.addToken(new Token(Symbol.toSymbol("("), "(", new Place("testParsePat5", 0, 1), TokenType.puncToken));
+        tokens.addToken(new Token(Symbol.toSymbol("f"), "f", new Place("testParsePat5", 0, 2), TokenType.varToken));
+        tokens.addToken(new Token(Symbol.toSymbol(")"), ")", new Place("testParsePat5", 0, 3), TokenType.puncToken));
+        Parser.setToks(tokens);
+        Pat result = Parser.parsePat();
+        compareParsedObjects(expResult, result);
+    }
+
+    /**
+     * Test parsePat with token stream containing two vars surrounded in parens
+     */
+    @Test
+    public void testParsePat6() {
+        System.out.println("Testing parsePat6");
+        ArrayList<Pat> expPats = new ArrayList<Pat>();
+        expPats.add(new PatVar(new Token(Symbol.toSymbol("f"), "f", new Place("testParsePat6", 0, 2), TokenType.varToken)));
+        expPats.add(new PatConst(new Token(Symbol.toSymbol("4"), "4", new Place("testParsePat6", 0, 4), TokenType.numberToken)));
+        PatTuple expResult = new PatTuple(expPats);
+        TokenStream tokens = new TokenStream();
+        tokens.addToken(new Token(Symbol.toSymbol("("), "(", new Place("testParsePat6", 0, 1), TokenType.puncToken));
+        tokens.addToken(new Token(Symbol.toSymbol("f"), "f", new Place("testParsePat6", 0, 2), TokenType.varToken));
+        tokens.addToken(new Token(Symbol.toSymbol(","), ",", new Place("testParsePat6", 0, 3), TokenType.puncToken));
+        tokens.addToken(new Token(Symbol.toSymbol("4"), "4", new Place("testParsePat6", 0, 4), TokenType.numberToken));
+        tokens.addToken(new Token(Symbol.toSymbol(")"), ")", new Place("testParsePat6", 0, 5), TokenType.puncToken));
+        Parser.setToks(tokens);
+        Pat result = Parser.parsePat();
+        compareParsedObjects(expResult, result);
+    }
+
+    /**
+     * Test parsePat with token stream containing two vars surrounded in brackets
+     */
+    @Test
+    public void testParsePat7() {
+        System.out.println("Testing parsePat7");
+        ArrayList<Pat> expPats = new ArrayList<Pat>();
+        expPats.add(new PatVar(new Token(Symbol.toSymbol("f"), "f", new Place("testParsePat7", 0, 2), TokenType.varToken)));
+        expPats.add(new PatConst(new Token(Symbol.toSymbol("4"), "4", new Place("testParsePat7", 0, 4), TokenType.numberToken)));
+        PatList expResult = new PatList(expPats);
+        TokenStream tokens = new TokenStream();
+        tokens.addToken(new Token(Symbol.toSymbol("["), "[", new Place("testParsePat7", 0, 1), TokenType.puncToken));
+        tokens.addToken(new Token(Symbol.toSymbol("f"), "f", new Place("testParsePat7", 0, 2), TokenType.varToken));
+        tokens.addToken(new Token(Symbol.toSymbol(","), ",", new Place("testParsePat7", 0, 3), TokenType.puncToken));
+        tokens.addToken(new Token(Symbol.toSymbol("4"), "4", new Place("testParsePat7", 0, 4), TokenType.numberToken));
+        tokens.addToken(new Token(Symbol.toSymbol("]"), "]", new Place("testParsePat7", 0, 5), TokenType.puncToken));
+        Parser.setToks(tokens);
+        Pat result = Parser.parsePat();
+        compareParsedObjects(expResult, result);
+    }
+
+    /**
+     * Test of parseAExp method, using a numberToken.
+     */
+    @Test
+    public void testParseAExp() {
+        System.out.println("parseAExp");
+        Token t1 = new Token(Symbol.toSymbol("42"), "42", new Place("testParseAExp", 0, 1), TokenType.numberToken);
+        TokenStream tokens = new TokenStream();
+        tokens.addToken(t1);
+        Parser.setToks(tokens);
+        Exp expResult = new ExpConst(t1);
+        Exp result = Parser.parseAExp();
+        compareParsedObjects(expResult, result);
+    }
+
+    /**
+     * Tests parseAExp method with a varToken
+     */
+    @Test
+    public void testParseAExp2(){
+        System.out.println("parseAExp2");
+        Token t1 = new Token(Symbol.toSymbol("asdf"), "asdf", new Place("testParseAExp", 0, 1), TokenType.varToken);
+        TokenStream tokens = new TokenStream();
+        tokens.addToken(t1);
+        Parser.setToks(tokens);
+        Exp expResult = new ExpVar(t1);
+        Exp result = Parser.parseAExp();
+        compareParsedObjects(expResult, result);
     }
 
     /**
@@ -182,10 +270,10 @@ public class ParserTest {
      * Test of parseAExp method, of class Parser.
      */
     @Test
-    public void testParseAExp() {
-        System.out.println("parseAExp");
+    public void testParseExp() {
+        System.out.println("parseExp");
         Exp expResult = null;
-        Exp result = Parser.parseAExp();
+        Exp result = Parser.parseExp();
         assertEquals(expResult, result);
         // TODO review the generated test code and remove the default call to fail.
         fail("The test case is a prototype.");
@@ -202,10 +290,10 @@ public class ParserTest {
         ArrayList errors = Lexer.lexString(program, fname);
         Symbol s = Symbol.equals;
         Parser.setToks(Lexer.tokens);
-        
-          System.out.println(Parser.toks.next());
-          System.out.println(Parser.toks.next());
-        
+
+        System.out.println(Parser.toks.next());
+        System.out.println(Parser.toks.next());
+
         Parser.requirePunct(s);
         // TODO review the generated test code and remove the default call to fail.
         //fail("The test case is a prototype.");
@@ -291,19 +379,6 @@ public class ParserTest {
     }
 
     /**
-     * Test of parsePat method, of class Parser.
-     */
-    @Test
-    public void testParsePat1() {
-        System.out.println("parsePat");
-        Pat expResult = null;
-        Pat result = Parser.parsePat();
-        assertEquals(expResult, result);
-        // TODO review the generated test code and remove the default call to fail.
-        fail("The test case is a prototype.");
-    }
-
-    /**
      * Test of parseExp method, of class Parser.
      */
     @Test
@@ -381,8 +456,9 @@ public class ParserTest {
         // TODO review the generated test code and remove the default call to fail.
         fail("The test case is a prototype.");
     }
-    
-    private void compareParsedObjects(ParsedObject expected, ParsedObject actual){
+
+    private void compareParsedObjects(ParsedObject expected, ParsedObject actual) {
         assertTrue(expected.compareTo(actual));
     }
 }
+
