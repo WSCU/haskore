8ce4b3ad99fc5bd19e629639f2b35d35e2ef4e05
cmpuncle@gmail.com <cmpuncle@gmail.com@91e22212-0c79-11df-8ecc-dda264a263c0>
2010-02-25 22:58:05 +0000

    removed Javadoc signatures

--- src/music/ExpIf.java
+++ src/music/ExpIf.java
@@ -9,7 +9,6 @@ public class ExpIf extends Exp {
     public Exp testExp;
     public Exp elseExp;
     public Exp thenExp;
-    //Javadoc by Chris-February 19,2010
     /**
      * @param testExp assigns the initial expression to test
      * @param thenExp assigns the expression to execute
@@ -25,7 +24,6 @@ public class ExpIf extends Exp {
     public boolean isIf() {
         return true;
     }
-    //Javadoc by Chris-February 19,2010
     /**
      * @param p compares the given ParsedObject to see if it is an if expression
      * @return gives results of the test, returns true if the Expressions are equal, false if it is not.
